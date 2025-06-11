5c12d22d291ecc1b454c7e2258b194c7f9038415
Masum <hydrapikachu29@gmail.com>
2025-06-12 01:12:33 +0530

    Added profile info part

--- app/src/main/java/com/masum/businesscard/MainActivity.kt
+++ app/src/main/java/com/masum/businesscard/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.masum.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +27,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BusinessCardTheme {
-
+                Surface (modifier = Modifier.fillMaxSize()) {
+                   profile()
+                }
             }
         }
     }
@@ -37,6 +43,16 @@ fun  profile() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        val image = painterResource(R.drawable.android_logo)
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = "Masum",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Android Developer",
+            fontSize = 20.sp
+        )
     }
 }
