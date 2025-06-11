5da632ebf795b614bb8395b41ec37357da9c3e5c
Masum <hydrapikachu29@gmail.com>
2025-06-11 21:43:08 +0530

    Basic properties of column in profile section

--- app/src/main/java/com/masum/businesscard/MainActivity.kt
+++ app/src/main/java/com/masum/businesscard/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.masum.businesscard.ui.theme.BusinessCardTheme
 
@@ -26,6 +30,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun  naming () {
-    
+fun  profile() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
 }
