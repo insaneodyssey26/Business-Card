168088f66471e9548b601e90933c25600d16ca6f
Masum <hydrapikachu29@gmail.com>
2025-06-13 00:56:54 +0530

    Added spacer between profile and contacts

--- app/src/main/java/com/masum/businesscard/MainActivity.kt
+++ app/src/main/java/com/masum/businesscard/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Email
@@ -50,6 +52,7 @@ fun BusinessCardApp() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         profile()
+        Spacer(modifier = Modifier.height(50.dp))
         contacts()
     }
 }
