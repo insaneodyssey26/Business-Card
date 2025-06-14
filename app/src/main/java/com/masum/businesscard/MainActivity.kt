2e10a531a2e9391956f53e317bd41cf78269dbd0
Masum <hydrapikachu29@gmail.com>
2025-06-15 01:07:03 +0530

    Removed unused imports

--- app/src/main/java/com/masum/businesscard/MainActivity.kt
+++ app/src/main/java/com/masum/businesscard/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,7 +25,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.masum.businesscard.ui.theme.BusinessCardTheme
