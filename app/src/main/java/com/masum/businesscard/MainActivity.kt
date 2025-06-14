0cf52c1d3dd75be495fbe994e1c179fdd8622166
Masum <hydrapikachu29@gmail.com>
2025-06-15 01:06:25 +0530

    Changed profile section texts to string resource

--- app/src/main/java/com/masum/businesscard/MainActivity.kt
+++ app/src/main/java/com/masum/businesscard/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -66,12 +67,12 @@ fun  Profile() {
         val image = painterResource(R.drawable.android_logo)
         Image(painter = image, contentDescription = null)
         Text(
-            text = "Masum",
+            text = stringResource(R.string.masum),
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold
         )
         Text(
-            text = "Android Developer",
+            text = stringResource(R.string.android_developer),
             fontSize = 20.sp
         )
     }
